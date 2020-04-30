c2e51a9f3c40290404503ce9380147da83e49fed
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-04-29 22:35:37 -0500

    Finish Exercise 13_1

--- exercises/exercise13_1/src/main/java/cs544/exercise13_1/App.java
+++ exercises/exercise13_1/src/main/java/cs544/exercise13_1/App.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App 
 {
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"springconfig.xml");
 		ICustomerService customerService = context.getBean("customerService",
